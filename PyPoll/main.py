03cac8d861ed741c39018696d3f414c1f39a3d58
Nemmans <76070320+Nemmans@users.noreply.github.com>
2021-01-28 16:58:46 -0600

    Update main.py

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,1 +1,88 @@
-print("Hello from PyPoll!")
+# Import mods
+import os
+import csv
+
+# Define variables
+total_votes = 0
+khan_votes = 0
+correy_votes = 0
+li_votes = 0
+otooley_votes = 0
+
+# Set path for file
+csvpath = os.path.join('Resources', 'election_data.csv')
+
+# Open & read CSV file
+with open(csvpath, newline='') as csvfile:
+
+    # CSV reader specifies delimiter & variable that holds contents
+    election_data = csv.reader(csvfile, delimiter=',')
+    
+    # Read header row 
+    csv_header = next(csvfile)
+
+    # Read data after header
+    for row in election_data:
+        
+        # Calculate total votes 
+        total_votes += 1
+        
+        # Calculate total votes each candidate won | Thank you TA's for starting me off
+        if (row[2] == "Khan"):
+            khan_votes += 1
+        elif (row[2] == "Correy"):
+            correy_votes += 1
+        elif (row[2] == "Li"):
+            li_votes += 1
+        else:
+            otooley_votes += 1
+            
+    # Calculate percentage of votes per candidate
+    kahn_percent = khan_votes / total_votes
+    correy_percent = correy_votes / total_votes
+    li_percent = li_votes / total_votes
+    otooley_percent = otooley_votes / total_votes
+    
+    # Calculate winner off popular vote
+    winner = max(khan_votes, correy_votes, li_votes, otooley_votes)
+
+    if winner == khan_votes:
+        winner_name = "Khan"
+    elif winner == correy_votes:
+        winner_name = "Correy"
+    elif winner == li_votes:
+        winner_name = "Li"
+    else:
+        winner_name = "O'Tooley" 
+
+# Print Analysis
+print(f"Election Results")
+print(f"---------------------------")
+print(f"Total Votes: {total_votes}")
+print(f"---------------------------")
+print(f"Kahn: {kahn_percent:.3%}({khan_votes})")
+print(f"Correy: {correy_percent:.3%}({correy_votes})")
+print(f"Li: {li_percent:.3%}({li_votes})")
+print(f"O'Tooley: {otooley_percent:.3%}({otooley_votes})")
+print(f"---------------------------")
+print(f"Winner: {winner_name}")
+print(f"---------------------------")
+
+# Specify file to write to
+output_file = os.path.join('Resources', 'election_data_revised.text')
+
+# Open file using "Write" Mode. Specify the variable to hold contents
+with open(output_file, 'w',) as txtfile:
+
+# Write new data | Thanks StackOverflow re: export txt file
+    txtfile.write(f"Election Results\n")
+    txtfile.write(f"---------------------------\n")
+    txtfile.write(f"Total Votes: {total_votes}\n")
+    txtfile.write(f"---------------------------\n")
+    txtfile.write(f"Kahn: {kahn_percent:.3%}({khan_votes})\n")
+    txtfile.write(f"Correy: {correy_percent:.3%}({correy_votes})\n")
+    txtfile.write(f"Li: {li_percent:.3%}({li_votes})\n")
+    txtfile.write(f"O'Tooley: {otooley_percent:.3%}({otooley_votes})\n")
+    txtfile.write(f"---------------------------\n")
+    txtfile.write(f"Winner: {winner_name}\n")
+    txtfile.write(f"---------------------------\n")
